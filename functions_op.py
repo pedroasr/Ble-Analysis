d5d961902d9bd4767847837ab2a024ea0be50c27
pedroasr96 <pedroantoniosr96@gmail.com>
2022-11-07 16:18:04 +0100

    Deleted idea

--- functions_op.py
+++ functions_op.py
@@ -351,50 +351,6 @@ def savePlotColumns(data, path="../figures/", path2="../figuresDate/"):
         plt.close()
 
 
-def checkTrainingSet(data):
-    """Función que comprueba que los valores obtenidos para el conjunto de entrenamiento tenga sentido. Devolverá un dataframe con los resultados"""
-
-    checkDataframe = pd.DataFrame(
-        columns=["Timestamp", "Maximum number of MAC", "Maximum number of Pair MAC", "Number of NaN"])
-
-    trainingSetCheck = data.copy()
-    trainingSetCheck["Timestamp"] = pd.to_datetime(trainingSetCheck["Timestamp"])
-    date = trainingSetCheck["Timestamp"][0].date().strftime('%Y-%m-%d')
-    trainingSetCheck = trainingSetCheck.loc[:,
-                       (trainingSetCheck.columns != "Timestamp") & (trainingSetCheck.columns != "Ocupacion") & (
-                                   trainingSetCheck.columns != "Minutes")]
-
-    nanNumber = trainingSetCheck.isna().sum()
-    print(nanNumber)
-
-    '''
-        group = group.fillna(0)
-        arr = group["N MAC TOTAL"] >= (group["N MAC RCE"] + group["N MAC RDE"] + group["N MAC RCDE"] + group["N MAC RBE"])
-
-        if len(set(arr)) == 1:
-            checkNumberPairMAC = "OK"
-        else:
-            checkNumberPairMAC = "ERROR"
-        boolCheck = []
-        for column in group.columns:
-            arr = group["N MAC TOTAL"] >= group[column]
-            if len(set(arr)) == 1:
-                boolCheck.append(True)
-            else:
-                boolCheck.append(False)
-
-        if len(set(boolCheck)) == 1:
-            checkNumberMAC = "OK"
-        else:
-            checkNumberMAC = "ERROR"
-
-        dt = pd.DataFrame([[date, checkNumberMAC, checkNumberPairMAC, nanNumber]],columns=["Timestamp", "Maximum number of MAC", "Maximum number of Pair MAC", "Number of NaN"])
-        checkDataframe = pd.concat([checkDataframe, dt])
-    print(checkDataframe)
-
-    return checkDataframe
-'''
-
 
 def getTrainingDataset(dataArray, personCountArray, stateArray):
     """Función que devuelve un conjunto de datos para el algoritmo de Machine Learning y un dataframe con los valores
@@ -457,7 +413,6 @@ def getTrainingDataset(dataArray, personCountArray, stateArray):
         trainingSet = pd.DataFrame(trainingData, columns=columns)
         trainingDataSet = pd.concat([trainingDataSet, trainingSet], ignore_index=True)
         savePlotColumns(trainingSet)
-        checkTrainingSet(trainingSet)
 
     trainingDataSet.to_csv("../docs/training-set.csv", sep=";", na_rep="NaN", index=False)
 
