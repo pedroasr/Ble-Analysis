e5a12fb0a970c67b099068822320751283cafa87
pedroasr96 <pedroantoniosr96@gmail.com>
2022-11-07 14:49:10 +0100

    Fixed Error Keep Alive MAC

--- pruebas.py
+++ pruebas.py
@@ -1,12 +1,13 @@
 from functions_op import *
 import pandas as pd
-import joblib
+# import joblib
 import warnings
+
 warnings.filterwarnings("ignore")
 
 pd.options.mode.chained_assignment = None
 
-
-dataArray, personCountArray, stateArray, dates = readDataFromDirectory("../docs/data_prueba", "../docs/personcount_prueba", "../docs/state_prueba")
+dataArray, personCountArray, stateArray = readDataFromDirectory("../docs/data_prueba", "../docs/personcount_prueba",
+                                                                "../docs/state_prueba")
 
 trainingDataset = getTrainingDataset(dataArray, personCountArray, stateArray)
