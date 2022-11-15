8911bfd895feba275dcfc21d94d7ed16acc54e86
pedroasr96 <pedroantoniosr96@gmail.com>
2022-11-15 17:43:24 +0100

    Correct path, RandomForestRegressor problem

--- train_models.py
+++ train_models.py
@@ -15,19 +15,19 @@ trainingSet = pd.read_csv("../docs/filled-training-set.csv", sep=";")
 X = trainingSet.loc[:, (trainingSet.columns != "Timestamp") & (trainingSet.columns != "Ocupacion")]
 y = trainingSet.loc[:, trainingSet.columns == "Ocupacion"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=288)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=288)
 
 # Primera opción de estimador -- HistGradientBoostingRegressor
 est = HistGradientBoostingRegressor().fit(X_train, y_train)
 print(f"R Square of HistGradientBoostingRegressor: {est.score(X_test, y_test):0.4f}")
-joblib.dump(est, 'models/HistGradientBoostingRegressor.pkl')
+joblib.dump(est, '../models/HistGradientBoostingRegressor.pkl')
 
 # Segunda opción de estimador -- LGBMRegressor
 lgbm = ltb.LGBMRegressor().fit(X_train, y_train)
 print(f"R Square of LGBMRegressor: {lgbm.score(X_test, y_test):0.4f}")
-joblib.dump(lgbm, 'models/LGBMRegressor.pkl')
+joblib.dump(lgbm, '../models/LGBMRegressor.pkl')
 
 # Tercera opción de estimador -- RandomForestRegressor
-rfr = RandomForestRegressor().fit(X_train, y_train)
-print(f"R Square of RandomForestRegressor: {rfr.score(X_test, y_test):0.4f}")
-joblib.dump(rfr, 'models/RandomForestRegressor.pkl')
+# rfr = RandomForestRegressor().fit(X_train, y_train)
+# print(f"R Square of RandomForestRegressor: {rfr.score(X_test, y_test):0.4f}")
+# joblib.dump(rfr, '../models/RandomForestRegressor.pkl')
