407de1f01a9a27ad1b1579538df56f5484a28995
pedroasr96 <pedroantoniosr96@gmail.com>
2022-11-28 15:16:41 +0100

    Script that is used for lazypredict

---
+++ select_algorithm_lazy.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from lazypredict.Supervised import LazyRegressor
+from sklearn.model_selection import train_test_split
+import warnings
+
+warnings.filterwarnings("ignore")
+
+trainingSet = pd.read_csv("../docs/filled-training-set.csv", sep=";")
+
+X = trainingSet.loc[:, (trainingSet.columns != "Timestamp") & (trainingSet.columns != "Ocupacion")]
+y = trainingSet.loc[:, trainingSet.columns == "Ocupacion"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=288)
+
+reg = LazyRegressor(verbose=0, ignore_warnings=True, custom_metric=None)
+
+models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+
+print(models)
