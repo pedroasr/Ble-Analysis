b653819e8c6f06b9810fdbacdcbc2ba4340ef028
pedroasr96 <pedroantoniosr96@gmail.com>
2022-12-07 20:56:22 +0100

    Comments added

--- cross_validation_graph.py
+++ cross_validation_graph.py
@@ -9,26 +9,31 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
+# Carga de datos.
 trainingSet = pd.read_csv("../docs/filled-training-set.csv", sep=";")
 trainingSet["Timestamp"] = pd.to_datetime(trainingSet["Timestamp"])
 dates = trainingSet["Timestamp"].dt.strftime('%Y-%m-%d %H:%M:%S')
 
+# División de datos.
 X = trainingSet.loc[:, (trainingSet.columns != "Timestamp") & (trainingSet.columns != "Ocupacion")]
 y = trainingSet.loc[:, trainingSet.columns == "Ocupacion"]
 
+# Creación de modelos con los hiperparámetros óptimos.
 est = ExtraTreesRegressor(n_estimators=500)
 rdf = RandomForestRegressor(n_estimators=600)
 xgbm = xgb.XGBRegressor(eta=0.14, max_depth=3, subsample=0.98)
 
+# Usando Cross Validation, devuelve los valores predichos.
 estPredict = cross_val_predict(est, X, y, cv=5).astype(int)
 rdfPredict = cross_val_predict(rdf, X, y, cv=5).astype(int)
 xgbmPredict = cross_val_predict(xgbm, X, y, cv=5).astype(int)
 
+# Se guardan los datos en un DataFrame.
 data = np.array(np.transpose([dates, y["Ocupacion"], estPredict, rdfPredict, xgbmPredict]))
-
 predDataframe = pd.DataFrame(data=data, columns=["Timestamp", "Ocupacion", "ExtraTreesRegressor", "RandomForestRegressor", "XGBRegressor"])
 predDataframe["Timestamp"] = pd.to_datetime(predDataframe["Timestamp"])
 
+# Se grafican los datos para cada día.
 for date in trainingSet["Timestamp"].dt.date.unique():
     group = predDataframe.loc[predDataframe["Timestamp"].dt.date == date]
     name = date.strftime("%Y-%m-%d")
