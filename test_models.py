62866dda7f721360b11e3ce87e9e20fa1aa1dd54
pedroasr96 <pedroantoniosr96@gmail.com>
2022-12-07 20:42:39 +0100

    Set as function and comments added again

--- test_models.py
+++ test_models.py
@@ -7,63 +7,74 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-testSet = pd.read_csv("../docs/filled-test-set.csv", sep=";")
 
-est = joblib.load('../models/ExtraTreesRegressor.pkl')
-xgbm = joblib.load('../models/XGBRegressor.pkl')
-rfr = joblib.load('../models/RandomForestRegressor.pkl')
+def testModels(pathTestSet, pathExtraTree, pathXGB, pathRandomForest, folder):
+    """Funcioón que carga los modelos entrenados y los aplica al conjunto de test."""
 
-testSet["Timestamp"] = pd.to_datetime(testSet["Timestamp"])
-dates = testSet["Timestamp"].dt.date.unique()
-dataArray = []
+    # Cargamos los datos.
+    testSet = pd.read_csv(pathTestSet, sep=";")
 
-for date in dates:
-    group = testSet.loc[testSet["Timestamp"].dt.date == date]
-    data = pd.DataFrame(group[["Timestamp", "Ocupacion"]], columns=["Timestamp", "Ocupacion"])
+    # Cargamos los modelos.
+    est = joblib.load(pathExtraTree)
+    xgbm = joblib.load(pathXGB)
+    rfr = joblib.load(pathRandomForest)
 
-    name = date.strftime("%Y-%m-%d")
+    testSet["Timestamp"] = pd.to_datetime(testSet["Timestamp"])
+    dates = testSet["Timestamp"].dt.date.unique()
+    dataArray = []
 
-    X = group.loc[:, (group.columns != "Timestamp") & (group.columns != "Ocupacion")]
+    # Para cada fecha disponible, se genera un gráfico con los datos reales y los predichos además del error producido.
+    for date in dates:
+        group = testSet.loc[testSet["Timestamp"].dt.date == date]
+        data = pd.DataFrame(group[["Timestamp", "Ocupacion"]], columns=["Timestamp", "Ocupacion"])
 
-    predicted_etr_y = est.predict(X)
-    predicted_xgbm_y = xgbm.predict(X)
-    predicted_rfr_y = rfr.predict(X)
+        name = date.strftime("%Y-%m-%d")
 
-    data["ExtraTreesRegressor"] = predicted_etr_y
-    data["XGBRegressor"] = predicted_xgbm_y
-    data["RandomForestRegressor"] = predicted_rfr_y
+        X = group.loc[:, (group.columns != "Timestamp") & (group.columns != "Ocupacion")]
 
-    dataArray.append(data)
+        # Se aplican los modelos.
+        predicted_etr_y = est.predict(X)
+        predicted_xgbm_y = xgbm.predict(X)
+        predicted_rfr_y = rfr.predict(X)
 
-    data["ErrorExtraTreesRegressor"] = np.absolute(predicted_etr_y - data["Ocupacion"])
-    data["ErrorXGBRegressor"] = np.absolute(predicted_xgbm_y - data["Ocupacion"])
-    data["ErrorRandomForestRegressor"] = np.absolute(predicted_rfr_y - data["Ocupacion"])
+        # Se añaden los datos predichos a la tabla.
+        data["ExtraTreesRegressor"] = predicted_etr_y
+        data["XGBRegressor"] = predicted_xgbm_y
+        data["RandomForestRegressor"] = predicted_rfr_y
 
-    fig, ax = plt.subplots(figsize=(10, 6))
-    date_form = DateFormatter("%H:%M")
-    ax.xaxis.set_major_formatter(date_form)
-    plt.plot(data["Timestamp"], data["Ocupacion"], label="Ocupacion", color="red")
-    plt.plot(data["Timestamp"], data["ExtraTreesRegressor"], label="ExtraTreesRegressor", color="blue")
-    plt.plot(data["Timestamp"], data["XGBRegressor"], label="XGBRegressor", color="green")
-    plt.plot(data["Timestamp"], data["RandomForestRegressor"], label="RandomForestRegressor", color="yellow")
-    plt.xlabel("Hora")
-    plt.ylabel("Ocupacion")
-    plt.legend()
-    plt.grid()
-    plt.title(name)
-    plt.savefig("../figuresPredict/prediction/" + name + ".jpg")
-    plt.clf()
-    plt.close()
+        dataArray.append(data)
 
-    plt.plot(data["Timestamp"], data["Ocupacion"], label="Ocupacion", color="red")
-    plt.plot(data["Timestamp"], data["ErrorExtraTreesRegressor"], label="ErrorExtraTreesRegressor", color="blue")
-    plt.plot(data["Timestamp"], data["ErrorXGBRegressor"], label="ErrorXGBRegressor", color="green")
-    plt.plot(data["Timestamp"], data["ErrorRandomForestRegressor"], label="ErrorRandomForestRegressor", color="yellow")
-    plt.xlabel("Hora")
-    plt.ylabel("Error")
-    plt.legend()
-    plt.grid()
-    plt.title(name)
-    plt.savefig("../figuresPredict/error/" + name + ".jpg")
-    plt.clf()
-    plt.close()
+        # Se añaden los datos del error a la tabla.
+        data["ErrorExtraTreesRegressor"] = np.absolute(predicted_etr_y - data["Ocupacion"])
+        data["ErrorXGBRegressor"] = np.absolute(predicted_xgbm_y - data["Ocupacion"])
+        data["ErrorRandomForestRegressor"] = np.absolute(predicted_rfr_y - data["Ocupacion"])
+
+        # Se generan los gráficos.
+        fig, ax = plt.subplots(figsize=(10, 6))
+        date_form = DateFormatter("%H:%M")
+        ax.xaxis.set_major_formatter(date_form)
+        plt.plot(data["Timestamp"], data["Ocupacion"], label="Ocupacion", color="red")
+        plt.plot(data["Timestamp"], data["ExtraTreesRegressor"], label="ExtraTreesRegressor", color="blue")
+        plt.plot(data["Timestamp"], data["XGBRegressor"], label="XGBRegressor", color="green")
+        plt.plot(data["Timestamp"], data["RandomForestRegressor"], label="RandomForestRegressor", color="yellow")
+        plt.xlabel("Hora")
+        plt.ylabel("Ocupacion")
+        plt.legend()
+        plt.grid()
+        plt.title(name)
+        plt.savefig(f"{folder}/prediction/" + name + ".jpg")
+        plt.clf()
+        plt.close()
+
+        plt.plot(data["Timestamp"], data["Ocupacion"], label="Ocupacion", color="red")
+        plt.plot(data["Timestamp"], data["ErrorExtraTreesRegressor"], label="ErrorExtraTreesRegressor", color="blue")
+        plt.plot(data["Timestamp"], data["ErrorXGBRegressor"], label="ErrorXGBRegressor", color="green")
+        plt.plot(data["Timestamp"], data["ErrorRandomForestRegressor"], label="ErrorRandomForestRegressor", color="yellow")
+        plt.xlabel("Hora")
+        plt.ylabel("Error")
+        plt.legend()
+        plt.grid()
+        plt.title(name)
+        plt.savefig(f"{folder}/error/" + name + ".jpg")
+        plt.clf()
+        plt.close()
