94a9f4dde691fb078b0c8bb07bd9566c2be7bd78
pedroasr96 <pedroantoniosr96@gmail.com>
2022-12-07 20:36:17 +0100

    Comments added

--- select_algorithm_lazy.py
+++ select_algorithm_lazy.py
@@ -5,13 +5,17 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
+# Carga de datos.
 trainingSet = pd.read_csv("../docs/filled-training-set.csv", sep=";")
 
+# División de datos.
 X = trainingSet.loc[:, (trainingSet.columns != "Timestamp") & (trainingSet.columns != "Ocupacion")]
 y = trainingSet.loc[:, trainingSet.columns == "Ocupacion"]
 
+# División de datos en entrenamiento y test.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=288)
 
+# Entrenamiento de modelos.
 reg = LazyRegressor(verbose=0, ignore_warnings=True, custom_metric=None)
 
 models, predictions = reg.fit(X_train, X_test, y_train, y_test)
